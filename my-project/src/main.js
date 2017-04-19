c21f0b3e8d3a04affd02a8ae6f2261baa1852a67
SJR\kadam <kadamon2007@gmail.com>
2017-04-19 20:29:27 +0900

    add ex10, ex11, ex12

--- my-project/src/main.js
+++ my-project/src/main.js
@@ -1,7 +1,9 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// The Vue build version to load with the `import` command;
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.;
 import Vue from 'vue'
 import App from './App'
+import axios from 'axios'
+import lodash from 'lodash.debounce'
 import router from './router'
 
 Vue.config.productionTip = false
@@ -13,6 +15,7 @@ new Vue({
   template: '<App/>',
   components: { App }
 })
+
 new Vue({
   el: '#app2',
   data: {
@@ -118,5 +121,92 @@ new Vue({
       // `this` 는 vm 인스턴스를 가리킵니다.
       return this.message.split('').reverse().join('')
     }
+  },
+  methods: {
+    reversedmessage: function () {
+      return this.message.split('').reverse().join('')
+    }
+  }
+})
+
+new Vue({
+  el: '#app-10',
+  data: {
+    firstName: 'Foo',
+    lastName: 'Bar',
+    fullName: 'Foo Bar'
+  },
+  watch: {
+    firstName: function (val) {
+      this.fullName = val + ' ' + this.lastName
+    },
+    lastName: function (val) {
+      this.fullName = this.firstName + ' ' + val
+    }
+  }
+})
+
+new Vue({
+  el: '#app-11',
+  data: {
+    firstName: 'seo',
+    lastName: 'Bar',
+    fullName: 'sed Bar'
+  },
+  computed: {
+    fullName: {
+    // getter
+      get: function () {
+        return this.firstName + ' ' + this.lastName
+      },
+    // setter
+      set: function (newValue) {
+        var names = newValue.split(' ')
+        this.firstName = names[0]
+        this.lastName = names[names.length - 1]
+      }
+    }
+  }
+})
+
+new Vue({
+  el: '#app-12',
+  data: {
+    question: '',
+    answer: '질문을 하기 전까지는 대답할 수 없습니다.'
+  },
+  watch: {
+    // 질문이 변경될 때 마다 이 기능이 실행됩니다.
+    question: function (newQuestion) {
+      this.answer = '입력을 기다리는 중...'
+      this.getAnswer()
+    }
+  },
+  methods: {
+    // _.debounce는 lodash가 제공하는 기능으로
+    // 특히 시간이 많이 소요되는 작업을 실행할 수 있는 빈도를 제한합니다.
+    // 이 경우, 우리는 yesno.wtf/api 에 액세스 하는 빈도를 제한하고,
+    // 사용자가 ajax요청을 하기 전에 타이핑을 완전히 마칠 때까지 기다리길 바랍니다.
+    // _.debounce 함수(또는 이와 유사한 _.throttle)에 대한
+    // 자세한 내용을 보려면 https://lodash.com/docs#debounce 를 방문하세요.
+    getAnswer: _.debounce(
+      function () {
+        if (this.question.indexOf('?') === -1) {
+          this.answer = '질문에는 일반적으로 물음표가 포함 됩니다. ;-)'
+          return
+        }
+        this.answer = '생각중...'
+        var vm = this
+        axios.get('https://yesno.wtf/api')
+          .then(function (response) {
+            vm.answer = _.capitalize(response.data.answer)
+          })
+          .catch(function (error) {
+            vm.answer = '에러! API 요청에 오류가 있습니다. ' + error
+          })
+      },
+      // 사용자가 입력을 기다리는 시간(밀리세컨드) 입니다.
+      500
+    )
   }
 })
